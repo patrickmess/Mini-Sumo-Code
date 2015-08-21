82e5c8566640f3fdf2171348c15356e32099b0f5
Patrick Messimer <patrickmessimer@gmail.com>
2015-08-21 13:16:28 -0700

    Update Sumo.cpp

--- Sumo.cpp
+++ Sumo.cpp
@@ -4,14 +4,15 @@
 #include <Arduino.h>
 #include <QTRSensors.h>
 #include <Sumo.h>
+//#include <Wire.h>
+//#include <LSM303.h>
 
-
-#define NUM_SENSORS   3     // number of sensors used
+#define NUM_SENSORS   4     // number of sensors used
 #define TIMEOUT       2500  // waits for 2500 microseconds for sensor outputs to go low
 #define EMITTER_PIN   QTR_NO_EMITTER_PIN     // emitter is controlled by digital pin 2
 
 
-QTRSensorsRC qtrrc((unsigned char[]) {0,1,14,15}, NUM_SENSORS, TIMEOUT, EMITTER_PIN);
+QTRSensorsRC qtrrc((unsigned char[]) {0,1,20,21}, NUM_SENSORS, TIMEOUT, EMITTER_PIN);
 unsigned int sensorValues[NUM_SENSORS];
 
 //Initialization Function
@@ -38,6 +39,8 @@ Sumo::Sumo()
 	pinMode(lmd, OUTPUT);
 	pinMode(lmp, OUTPUT);
 	
+	//Setup Accelerometer
+	Wire.begin();
 }
 
 
@@ -85,25 +88,52 @@ void Sumo::ProxSensorRead()
 	//False is for no object detected
 	//True is for object detected
 	
-	//set default value to false for all values in array
-	for(int i = 0; i <= 4; i++)
-	{
-			proxSensors[i] = false;				//Set value to false
-	}
-													
 	const int diff = p1;	//"diff" for difference	
 							//Variable that stores the difference between 0 and the pin number
 							//of the first proximity sensor
 							//Used to relate the sensor pin number to the corresponding array position
 	
-	//Read each Sensor Value and determine whether or not an object has been detected
+	//set default value to true for all values in array
+	for(int i = 0; i < 4; i++)
+	{
+		proxSensors[i] = true;		//Set the corresponding array position to true
+											//If all the readings of the sensor are LOW
+											//Then we know that we are detecting an object
+											//If any are HIGH, then we are getting an intermittent
+											//which means there is no object in range of the sensor
+											//so we will change the array position to false
+		//Serial.print(i);				//For Debugging
+		//Serial.println(" = True");	//For Debugging
+	}
+	
+	//Read each Sensor Value 10 times and determine whether or not an object has been detected
 	//Then change the corresponding array position to indicate that
 	for(int i = p1; i <= p4; i++)
 	{
-		if (digitalRead(i) == LOW)  	//Sensor is read and an Object is Detected!
-			proxSensors[i - diff] = true;		//The corresponding array position changed to true
-		//Otherwise nothing happens
-		//The corresponding array position is set to false
+		 //Serial.println();				//For Debugging
+		 //Serial.println(i);				//For Debugging
+		 //Serial.println(i - diff);		//For Debugging
+		//Read the sensor 20 times
+		for(int ii = 0; ii < 30; ii++)
+		{
+			//Serial.print("-");		//For Debugging
+			//Serial.print(ii);			//For Debugging
+			if (digitalRead(i) == HIGH)		//Sensor reads that there is not an object		
+			{			
+				proxSensors[i - diff] = false;		//The corresponding array position changed to false
+				//Serial.println("false");	//For Debugging
+			}
+			else
+			{
+				//Do Nothing
+				//Serial.println("true");		//For Debugging
+			}
+				
+			delay(1);	//wait before reading the sensor again
+		}
+		//If all the readings were LOW, indicating that an object was detected
+		//then the array position will remain with a true value
+		//Otherwise, the array will array position will read false
 	}
 }
 
@@ -115,11 +145,11 @@ bool Sumo::readButton1()
 	buttonState = digitalRead(button1);		//Read the button state
 	if (buttonState == HIGH)				//If the button is pressed
 	{
-		while(buttonState == HIGH)				//wait until the button is released
+		/*while(buttonState == LOW)				//wait until the button is released
 		{
 			//check if button is released
 			buttonState = digitalRead(button1);					
-		}	
+		}	*/
 		return true;	//return true: indicating that the button has been pressed and released
 	}
 	
@@ -138,13 +168,13 @@ bool Sumo::readButton2()
 	buttonState = digitalRead(button2);		//Read the button state
 	if (buttonState == HIGH)				//If the button is pressed
 	{
-		while(buttonState == HIGH)				//wait until the button is released
+		/*while(buttonState == HIGH)				//wait until the button is released
 		{
 			//check if button is released
 			buttonState = digitalRead(button2);					
 		}
-		delay(500);		//wait 1/2 second		
-		return true;	//return true: indicating that the button has been pressed and released
+		delay(500);		//wait 1/2 second		*/
+		return true;	//return true: indicating that the button has been pressed
 	}
 	
 	else	//If the button is not pressed
@@ -159,7 +189,7 @@ bool Sumo::readButton2()
 //0 is for no motor movement
 void Sumo::Drive(int left, int right)
 {
-	const int pwmMAX = 255; 	//The pwm pin sends a value on the range of 0 to 255
+	const double pwmMAX = 255; 	//The pwm pin sends a value on the range of 0 to 255
 	const double factor = pwmMAX / 100; 	//The factor that the input will need to be multiplied by
 										//in order to convert a base 100 value to a base 255 value
 										//equal to 2.55 when pwm range is 0 to 255
@@ -170,14 +200,14 @@ void Sumo::Drive(int left, int right)
 	if(left > 0)		//If the left input value is positive
 	{
 		digitalWrite(lmd, HIGH);		//set left direction to forwards	
-		int powerSignal = left * factor;	//Calculate value to send across pwm to
+		double powerSignal = left * factor;	//Calculate value to send across pwm to
 											 // motor driver for power													 
 		analogWrite(lmp, powerSignal);	//Set motor speed to calculated value		
 	}
 	else				//If the left input value is negative 
 	{
-		digitalWrite(lmd, HIGH);		//set left direction to backwards
-		int powerSignal = -1 * left * factor;	//Calculate value to send across pwm to
+		digitalWrite(lmd, LOW);		//set left direction to backwards
+		double powerSignal = -1 * left * factor;	//Calculate value to send across pwm to
 											 // motor driver for power
 												/*////////////IMPORTANT/////////////////
 												left is multiplied by -1 in this equation 
@@ -190,14 +220,14 @@ void Sumo::Drive(int left, int right)
 	if(right > 0)		//If the right input value is positive
 	{
 		digitalWrite(rmd, HIGH);		//set right direction to forwards
-		int powerSignal = right * factor;	//Calculate value to send across pwm to
+		double powerSignal = right * factor;	//Calculate value to send across pwm to
 											 // motor driver for power											 
 		analogWrite(rmp, powerSignal);	//Set motor speed to calculated value		
 	}
 	else				//If the right input value is negative 
 	{
-		digitalWrite(rmd, HIGH);		//set right direction to backwards
-		int powerSignal = -1 * right * factor;	//Calculate value to send across pwm to
+		digitalWrite(rmd, LOW);		//set right direction to backwards
+		double powerSignal = -1 * right * factor;	//Calculate value to send across pwm to
 											 // motor driver for power
 												/*////////////IMPORTANT/////////////////
 												right is multiplied by -1 in this equation 
@@ -220,3 +250,29 @@ void Sumo::CalibrateLight()
 	digitalWrite(led, LOW);     // turn off Yellow LED to indicate we are done with the calibration
 }
 
+//Function to read the Accelerometer
+void Sumo::AccelRead()
+{
+	//Set Accel to 0
+	int Accel = 0;		//Set Accel to 0
+	int array[100];		//Create an array with 100 integers
+	
+	//read the accelerometer 100 times, storing values in array
+	for(int i = 0; i < 50; i++)
+	{
+		compass.read();
+		array[i] = compass.a.x;
+		//delay(1);
+	}
+	
+	//Average those values
+	long total = 0;
+	int ii = 0;
+	while (ii < 100)
+	{
+		total = total + array[ii];
+		ii++;
+	}	
+	
+	Accel = total / ii;		//set Accel to that averaged value
+}
